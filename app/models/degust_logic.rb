drpowell/degust

commit 687b317467cea681d0ad35ae234dc310c0c0e3ba

Display R versions with "show r code"

app/models/degust_logic.rb
@@ -26,13 +26,16 @@ class DegustLogic
 
         return nil if method.nil?
 
-        input = ApplicationController.render(template: "degust/#{method}.R.erb", assigns: params, layout: false)
-        return input
+        ApplicationController.render(template: "degust/#{method}.R.erb", assigns: params, layout: false)
     end
 
-    def self.run_r_code(de_setting, query)
+    def self.get_versions_code()
+        ApplicationController.render(template: "degust/versions.R.erb", layout: false)
+    end
+
+    def self.run_r_code(make_code)
         tempfile = Dir.mktmpdir("R-tmp", "#{Rails.root.to_s}/tmp/")
-        code = get_r_code(de_setting, query, tempfile)
+        code = make_code.call(tempfile)
 
         sout = serr = exit_status = nil
         Open3.popen3('R','-q','--vanilla') do |stdin, stdout, stderr, wait_thr|
@@ -46,12 +49,23 @@ class DegustLogic
             return {error: {input: code, msg: serr, stdout: sout, exit_status: exit_status}}
         end
 
-        output = File.read(tempfile + '/output.txt')
-        extra = JSON.parse(File.read(tempfile +"/extra.json"))
-        res = {csv: output, extra: extra }
+        output = ""
+        begin
+            output = File.read(tempfile + '/output.txt')
+        rescue Errno::ENOENT
+            # Ignore
+        end
+
+        extra = ""
+        begin
+            JSON.parse(File.read(tempfile +"/extra.json"))
+        rescue Errno::ENOENT
+            # Ignore
+        end
+
+        res = {csv: output, extra: extra, stdout: sout, stderr: serr }
         FileUtils.remove_dir(tempfile)
 
-        #req.app.utility.caching.store(cacheKey, outStr);
         return res
     end
 

app/controllers/degust_controller.rb
@@ -80,7 +80,8 @@ class DegustController < ApplicationController
         cache = ActiveSupport::Cache::FileStore.new("#{Rails.root.to_s}/tmp/R-cache", expires_in: 7.days)
         json = cache.fetch(cacheKey) do
                   logger.info "Not cached."
-                  DegustLogic.run_r_code(de_setting, params)
+                  make_code = lambda {|tempfile| DegustLogic.get_r_code(de_setting, params, tempfile)}
+                  DegustLogic.run_r_code(make_code)
                end
         render json: json
     end
@@ -88,7 +89,8 @@ class DegustController < ApplicationController
     def dge_r_code
         de_setting = DeSetting.find_by_secure_id(params[:id])
         str = DegustLogic.get_r_code(de_setting, params, 'output_dir', false)
-        render plain: str
+        json = DegustLogic.run_r_code( lambda{|tempfile| DegustLogic.get_versions_code()} )
+        render plain: str + json[:stdout].html_safe
     end
 
     require 'csv'

commit ccea3e9cd7e71fff0f3d6dc05eed3ea7be259740

Fix bug introduced in 687b3174 that stopped "extra" being populated

app/models/degust_logic.rb
@@ -58,7 +58,7 @@ class DegustLogic
 
         extra = ""
         begin
-            JSON.parse(File.read(tempfile +"/extra.json"))
+            extra = JSON.parse(File.read(tempfile +"/extra.json"))
         rescue Errno::ENOENT
             # Ignore
         end