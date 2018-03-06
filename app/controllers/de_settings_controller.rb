791825495be7e1f0bb921fa1a25b131af6e31a87
David Powell <david@drp.id.au>
2018-03-06 23:36:03 +1100

    Rails backend can now respond with json to /visited query

--- app/controllers/de_settings_controller.rb
+++ app/controllers/de_settings_controller.rb
@@ -1,43 +1,50 @@
 class DeSettingsController < ApplicationController
-  def new
-  end
+    def new
+    end
 
-  def show
-      redirect_to degust_page_path("compare.html")+"?code="+params['id']
-  end
+    def show
+        redirect_to degust_page_path("compare.html")+"?code="+params['id']
+    end
 
-  def create
-    f = params['filename']
-    @user_file = UserFile.create()
-    @user_file.from_tempfile(f)
-    @user_file.save!
+    def create
+        f = params['filename']
+        @user_file = UserFile.create()
+        @user_file.from_tempfile(f)
+        @user_file.save!
 
-    @de_setting = DeSetting.new()
-    @de_setting.user_file = @user_file
-    @de_setting.user = current_user
-    @de_setting.save!
+        @de_setting = DeSetting.new()
+        @de_setting.user_file = @user_file
+        @de_setting.user = current_user
+        @de_setting.save!
 
-    redirect_to degust_page_path("compare.html")+"?code="+@de_setting.secure_id
-  end
+        redirect_to degust_page_path("compare.html")+"?code="+@de_setting.secure_id
+    end
 
-  def destroy
-      de_setting = DeSetting.find_by_secure_id(params[:id])
-      if !current_user.nil? && de_setting.user == current_user
-          Visited.where(:de_setting => de_setting).map {|v| v.destroy}
-          de_setting.destroy
-          redirect_to :back, :alert => "Data deleted!"
-      else
-          redirect_to :back, :alert => "Access denied!"
-      end
-  end
+    def destroy
+        de_setting = DeSetting.find_by_secure_id(params[:id])
+        if !current_user.nil? && de_setting.user == current_user
+            Visited.where(:de_setting => de_setting).map {|v| v.destroy}
+            de_setting.destroy
+            redirect_to :back, :alert => "Data deleted!"
+        else
+            redirect_to :back, :alert => "Access denied!"
+        end
+    end
 
-  def index
-      if current_user.nil?
-          redirect_to root_path
-      else
-          @mine = current_user.de_settings
-          @others = Visited.where(:user => current_user).map(&:de_setting).select {|de| de.user != current_user}
-      end
-  end
+    def index
+        if current_user.nil?
+            respond_to do |format|
+                format.html { redirect_to root_path }
+                format.json { render :json => { :redirect => root_path } }
+            end
+        else
+            @mine = current_user.de_settings
+            @others = Visited.where(:user => current_user).map(&:de_setting).select {|de| de.user != current_user}
 
+            respond_to do |format|
+                format.html
+                format.json { render :json => { :mine => @mine, :others => @others } }
+            end
+        end
+    end
 end
