b0bd7253595d24127c8f0f7b8859418f181ab4f9
David Powell <david@drp.id.au>
2018-08-16 17:17:47 +1000

    Make deleting genelists work

--- app/controllers/gene_lists_controller.rb
+++ app/controllers/gene_lists_controller.rb
@@ -1,8 +1,8 @@
 class GeneListsController < ApplicationController
     def index
         de_setting = DeSetting.find_by_secure_id(params[:id])
-        genelists = GeneList.where(:de_setting_id => de_setting.id).select([:title, :description])
-        render json: genelists
+        gene_lists = GeneList.where(:de_setting_id => de_setting.id).select([:id, :title, :description, :id_type])
+        render json: gene_lists
     end
 
     def show
@@ -32,12 +32,32 @@ class GeneListsController < ApplicationController
 
     def destroy
         de_setting = DeSetting.find_by_secure_id(params[:id])
-        genelist = de_settings.find_by_id(params[:gene_list_id])
-        if !current_user.nil? && de_setting.user == current_user && genelist.de_setting==de_setting
-            genelist.destroy
-            redirect_to :back, :alert => "Gene List deleted!"
+        gene_list = GeneList.find_by_id(params[:gene_list_id])
+
+        if !de_setting || !gene_list
+            render json: {
+                status: 403,
+                message: "No such object"
+              }.to_json
+            return
+        end
+
+        p de_setting
+        p gene_list
+
+        if de_setting.can_modify(current_user) && gene_list.de_setting==de_setting
+            gene_list.destroy
+            render json: {
+                status: 200,
+                message: "Gene list deleted"
+              }.to_json
+            return
         else
-            redirect_to :back, :alert => "Access denied!"
+            render json: {
+                status: 403,
+                message: "Access denied"
+              }.to_json
+            return
         end
     end
 
