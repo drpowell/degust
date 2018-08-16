919f9b1d30e94412ebddf27870a230488a539de6
David Powell <david@drp.id.au>
2018-08-16 16:17:09 +1000

    Add genelists routes to versioned API

--- config/routes.rb
+++ config/routes.rb
@@ -15,33 +15,44 @@ Rails.application.routes.draw do
   get '/degust', to: redirect('/')
 
   # Routes for degust app
-  resources :degust, only: [] do
-    member do
-      get 'settings'
-      post 'settings' => 'degust#save_settings'
-      get 'partial_csv'
-      get 'csv'
-      get 'dge'
-      get 'dge_r_code'
-      get 'kegg_titles'
-      resources :gene_lists, param: :gene_list_id
+  scope '/degust' do
+    # Default API for /degust
+    resources :degust, only: [], path: '' do
+      member do
+        get 'settings'
+        post 'settings' => 'degust#save_settings'
+        get 'partial_csv'
+        get 'csv'
+        get 'dge'
+        get 'dge_r_code'
+        get 'kegg_titles'
+        resources :gene_lists, param: :gene_list_id
+      end
     end
-  end
 
-  # Specific Version routes - don't know the rails way to do this...
-  constraints version: /[^\/]+/ do
-    get '/degust/version/:version/:id/settings' => 'degust#settings'
-    post '/degust/version/:version/:id/settings' => 'degust#save_settings'
-    get '/degust/version/:version/:id/partial_csv' => 'degust#partial_csv'
-    get '/degust/version/:version/:id/csv' => 'degust#csv'
-    get '/degust/version/:version/:id/dge' => 'degust#dge'
-    get '/degust/version/:version/:id/dge_r_code' => 'degust#dge_r_code'
-    get '/degust/version/:version/:id/kegg_titles' => 'degust#kegg_titles'
-    get '/degust/version/:version/kegg/*page' => 'degust#static_kegg'
-    get '/degust/version/:version/*page' => 'degust#static', :as => :degust_page_version
-  end
+    # Versioned API
+    constraints version: /[^\/]+/ do
+      scope 'version/:version/' do
+        resources :degust, only: [], path: '' do
+          member do
+            get 'settings'
+            post 'settings' => 'degust#save_settings'
+            get 'partial_csv'
+            get 'csv'
+            get 'dge'
+            get 'dge_r_code'
+            get 'kegg_titles'
+            resources :gene_lists, param: :gene_list_id
+          end
+        end
+        get '*page' => 'degust#static', :as => :degust_page_version
+      end
+    end  # End version constraint
 
-  # General calls
-  get '/degust/kegg/*page' => 'degust#static_kegg'
-  get '/degust/*page' => 'degust#static', :as => :degust_page
+    # General calls
+    get '/kegg/*page' => 'degust#static_kegg'
+    get '/*page' => 'degust#static', :as => :degust_page
+
+  end # End /degust scope
+
 end
