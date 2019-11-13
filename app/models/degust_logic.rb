b2ed48fa06fb07c6eda3b15d69230080a7e33ed8
David Powell <david@drp.id.au>
2019-11-14 10:37:10 +1100

    Add a 120sec timeout to running R code.
    - Also cleanup the R-tmp files even on error.  Used to keep these around for debugging

--- app/models/degust_logic.rb
+++ app/models/degust_logic.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'timeout'
 
 class DegustLogic
 
@@ -60,13 +61,24 @@ class DegustLogic
         tempfile = Dir.mktmpdir("R-tmp", "#{Rails.root.to_s}/tmp/")
         code = make_code.call(tempfile)
 
-        sout = serr = exit_status = nil
+        sout = serr = exit_status = timeout = nil
         Open3.popen3('R','-q','--vanilla') do |stdin, stdout, stderr, wait_thr|
-            stdin.write(code)
-            stdin.close_write
-            exit_status = wait_thr.value
-            sout = stdout.read
-            serr = stderr.read
+            begin
+                Timeout.timeout(120) do   # R has to complete running within this time
+                    stdin.write(code)
+                    stdin.close_write
+                    exit_status = wait_thr.value
+                    sout = stdout.read
+                    serr = stderr.read
+                end
+            rescue Timeout::Error => e
+                STDERR.puts "Timeout ruby run : #{e.inspect}"
+                timeout = true
+                Process.kill("KILL", wait_thr.pid)
+            end
+        end
+        if (timeout)
+            return {error: {input: code, msg: "R run timed out", stdout: sout, exit_status: exit_status}}
         end
         if (exit_status.exitstatus != 0)
             return {error: {input: code, msg: serr, stdout: sout, exit_status: exit_status}}
@@ -87,9 +99,9 @@ class DegustLogic
         end
 
         res = {csv: output, extra: extra, stdout: sout, stderr: serr }
-        FileUtils.remove_dir(tempfile)
-
         return res
+    ensure
+        FileUtils.remove_dir(tempfile)
     end
 
 private
